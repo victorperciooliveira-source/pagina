6d06c86eaaf82858193dee19021d87b68c5921ef
victorpercio <victorperciodeoliveira@gmail.com>
2025-08-11 22:04:02 -0300

    01

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+document.getElementById('changeTextBtn').addEventListener('click', function() {
+    const section = document.querySelector('section');
+    section.querySelector('p').textContent = 'Você clicou no botão! Texto alterado.';
+});
 
